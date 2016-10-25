2be930792deb0b3bd6adf1015a309b2125b8e18e
Tobias H <tobias.haussener@mobi.ch>
2016-10-25 20:18:15 +0200

    Add files via upload

---
+++ SozNum/SozNum/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace SozNum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lblABC = new System.Windows.Forms.Label();
+            this.btnValidate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(117, 31);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(151, 20);
+            this.tbInput.TabIndex = 0;
+            // 
+            // lblABC
+            // 
+            this.lblABC.AutoSize = true;
+            this.lblABC.Location = new System.Drawing.Point(12, 31);
+            this.lblABC.Name = "lblABC";
+            this.lblABC.Size = new System.Drawing.Size(99, 13);
+            this.lblABC.TabIndex = 1;
+            this.lblABC.Text = "Nummer eingeben: ";
+            // 
+            // btnValidate
+            // 
+            this.btnValidate.Location = new System.Drawing.Point(117, 57);
+            this.btnValidate.Name = "btnValidate";
+            this.btnValidate.Size = new System.Drawing.Size(151, 23);
+            this.btnValidate.TabIndex = 2;
+            this.btnValidate.Text = "Validate";
+            this.btnValidate.UseVisualStyleBackColor = true;
+            this.btnValidate.Click += new System.EventHandler(this.btnValidate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AHV-Nummer ist: ";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(114, 116);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnValidate);
+            this.Controls.Add(this.lblABC);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "AHV-Nummer überprüfen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lblABC;
+        private System.Windows.Forms.Button btnValidate;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SozNum/SozNum/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace SozNum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidate_Click(object sender, EventArgs e)
+        {
+            string cSozNummer = tbInput.Text;
+            Regex regex = new Regex(@"^[1-9]{3}\.[1-9]{4}\.[1-9]{4}\.[1-9]{2}$"); 
+            if (regex.IsMatch(cSozNummer))
+            {
+                lblOutput.Text = "Richtig";
+             }
+            else
+            {
+                lblOutput.Text = "Falsch";
+            }
+        }
+    }
+}
